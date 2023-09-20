bc678c79a5edd179d5de2c5743b05cd94f5b788d
happpy24 <vincentspeijer@gmail.com>
2023-09-20 09:19:58 +0200

    CSS + HTML Progression. Commit to add to desktop
    RAAAAAAAAAAH

--- PortfolioWeb/Controllers/HomeController.cs
+++ PortfolioWeb/Controllers/HomeController.cs
@@ -18,7 +18,22 @@ namespace PortfolioWeb.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Education()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Projects()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
         {
             return View();
         }
